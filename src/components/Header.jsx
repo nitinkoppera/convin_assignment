c2603a033b5105b8daea81285ef2311546d3ad3c
nitinkoppera <nitinkoppera001@gmail.com>
2023-03-29 00:18:49 +0530

    f1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 function Header(props) {
     return (
-        <div className='px-6 pt-5 pb-2 border-b border-black'>
+        <div className='px-6 pt-5 pb-2 border-b border-black flex justify-between'>
             <span className='text-3xl'>Convin Frontend Assignment</span>
+
+            <div className='flex'>
+                <div className='px-3 underline'><a href='/'>Home</a></div>
+                <div className='px-3 underline'><a href='/history'>History</a></div>
+            </div>
         </div>
     );
 }
